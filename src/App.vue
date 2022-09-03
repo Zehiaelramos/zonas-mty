5a9f950e047df577a7ecda18fe9baeba2dda1419
User Undefined <zehiaelderthev@gmail.com>
2022-09-03 17:16:07 -0500

    Change labnl config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/labnl.png" width="512px">
+    <HelloWorld msg="Zonas de Monterrey"/>
   </div>
 </template>
 
