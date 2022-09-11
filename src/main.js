285cecc0e9a1b8faa814ea6613e99f8162bbc43e
User Undefined <zehiaelderthev@gmail.com>
2022-09-10 23:58:53 -0500

    Change vue-router declaration

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import router from "./router"
 
 import VueRouter from 'vue-router'
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+Vue.use(VueRouter)
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
