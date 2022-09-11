530645b5ece4e37177e6f11e55b5cf4f557fa8b8
User Undefined <zehiaelderthev@gmail.com>
2022-09-10 22:24:26 -0500

    Add router-view navegation

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,31 @@
 
     <navbar-wrap/>
 
+    <!--
     <img alt="Vue logo" src="./assets/labnl.png" width="512px">
+    -->
 
-    <HelloWorld msg="Zonas de Monterrey"/>
+    <router-view
+      @routerViewReload="reload"
+      :key="state ? $route.fullPath : [$route.fullPath, value]"
+    />
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import NavbarWrap from './layout/NavbarWrap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     NavbarWrap,
   }
 }
 </script>
 
 <style>
-#app {
+#app-style {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
