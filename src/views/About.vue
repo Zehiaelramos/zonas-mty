902d2694fc8d3a43fde16c4d04fb7946c68df4e5
User Undefined <zehiaelderthev@gmail.com>
2022-09-10 22:18:48 -0500

    Add About view

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container mt-2 mb-4">
+    <input
+      type="text"
+      v-model="message"
+    >
+    <h2 class="message">{{ message }}</h2>
+    <h3>Some Other Message</h3>
+    <h4>One More Message</h4>
+    <button class="btn btn-primary">Submit!</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AboutWrap",
+    data() {
+      return {
+        message: '213',
+      }
+    },
+  }
+</script>
